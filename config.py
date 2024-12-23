7f67f9ae9dff62c0328143f1f5c2aaa76335374d
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-12-23 11:16:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Mrcutex1/Alexar",
+    "https://github.com/Mrcutex1/acer",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
