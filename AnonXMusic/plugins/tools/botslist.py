fd2b0d7378e5fb6b7632128adf4c09d71f99d52f
Its-Nexio <imrealsanatani@gmail.com>
2025-01-11 22:49:48 +0530

    All Set !

--- AnonXMusic/plugins/tools/botslist.py
+++ AnonXMusic/plugins/tools/botslist.py
@@ -13,17 +13,14 @@ async def bots(client, message):
         admin_bots = []
         non_admin_bots = []
         
-        # Iterate through the bots in the chat
         async for bot in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.BOTS):
             if bot.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
                 admin_bots.append(bot.user)
             else:
                 non_admin_bots.append(bot.user)
         
-        # Constructing the message
         text3 = f"<b>⬤ ʙᴏᴛ ʟɪsᴛ ➠</b> {message.chat.title}\n\n"
 
-        # Admin bots section
         if admin_bots:
             text3 += "<b>⬤ 🤖 ᴀᴅᴍɪɴ ʙᴏᴛs</b>\n"
             for bot in admin_bots[:-1]:
@@ -32,7 +29,6 @@ async def bots(client, message):
         else:
             text3 += "<b>⬤ 🤖 ᴀᴅᴍɪɴ ʙᴏᴛs ➠ ɴᴏɴᴇ</b>\n\n"
 
-        # Non-admin bots section
         if non_admin_bots:
             text3 += "<b>⬤ 🤖 ɴᴏɴ-ᴀᴅᴍɪɴ ʙᴏᴛs</b>\n"
             for bot in non_admin_bots[:-1]:
@@ -40,8 +36,7 @@ async def bots(client, message):
             text3 += f"<b>└</b> @{non_admin_bots[-1].username}\n\n"
         else:
             text3 += "<b>⬤ 🤖 ɴᴏɴ-ᴀᴅᴍɪɴ ʙᴏᴛs ➠ ɴᴏɴᴇ</b>\n\n"
-
-        # Total bot count
+            
         total_bots = len(admin_bots) + len(non_admin_bots)
         text3 += f"<b>⬤ ᴛᴏᴛᴀʟ ɴᴜᴍʙᴇʀ ᴏғ ʙᴏᴛs ➠</b> {total_bots}"
 
